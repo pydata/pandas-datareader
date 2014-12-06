0943292cca26736855a3e6e7dba6b24f88cfd350
Andy Hayden <andyhayden1@gmail.com>
2014-12-06 11:48:39 -0800

    Make into a package
    TODO
    * flesh out README
    * confirm license, I think it's BSD-2?

---
+++ pandas_data_readers/__init__.py
@@ -0,0 +1,1 @@
+__version__ = version = '0.1a'
---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ast import parse
+import os
+from setuptools import setup
+
+
+NAME = 'pandas_data_readers'
+
+
+def version():
+    """Return version string."""
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                           'pandas_data_readers',
+                           '__init__.py')) as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return parse(line).body[0].value.s
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+INSTALL_REQUIRES = (
+    ['pandas']
+)
+
+setup(
+    name=NAME,
+    version=version(),
+    description="Data readers extracted from the pandas codebase,"
+                "should be compatible with recent pandas versions",
+    long_description=readme(),
+    license='MIT License',
+    author='Andy Hayden',
+    author_email='andyhayden1@gmail.com',
+    url='https://github.com/hayd/pandas_data_readers',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='data',
+    install_requires=INSTALL_REQUIRES,
+    packages=['pandas_data_readers'],
+    test_suite='tests',
+    zip_safe=False,
+)
