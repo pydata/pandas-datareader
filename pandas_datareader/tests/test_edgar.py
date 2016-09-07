38ba60ff51148d80f8824b5d6a0daa3b1230ea7a
femtotrader <femto.trader@gmail.com>
2016-09-07 21:14:17 +0200

    TST Fix Edgar tests (#227)

--- pandas_datareader/tests/test_edgar.py
+++ pandas_datareader/tests/test_edgar.py
@@ -9,32 +9,32 @@ class TestEdgarIndex(tm.TestCase):
     def test_get_full_index(self):
         try:
             ed = web.DataReader('full', 'edgar-index')
+            assert len(ed) > 1000
         except RemoteDataError as e:
             raise nose.SkipTest(e)
-        assert len(ed > 1000)
 
     def test_get_nonzip_index_and_low_date(self):
         try:
             ed = web.DataReader('daily', 'edgar-index', '1994-06-30',
                                 '1994-07-02')
+            assert len(ed) > 200
         except RemoteDataError as e:
             raise nose.SkipTest(e)
-        assert len(ed > 200)
 
     def test_get_gz_index_and_no_date(self):
         try:
             ed = web.DataReader('daily', 'edgar-index')
+            assert len(ed) > 2000
         except RemoteDataError as e:
             raise nose.SkipTest(e)
-        assert len(ed > 2000)
 
     def test_6_digit_date(self):
         try:
             ed = web.DataReader('daily', 'edgar-index', '1998-05-18',
                                 '1998-05-18')
+            assert len(ed) < 1200
         except RemoteDataError as e:
             raise nose.SkipTest(e)
-        assert len(ed < 1200)
 
 if __name__ == '__main__':
     nose.runmodule(argv=[__file__, '-vvs', '-x', '--pdb', '--pdb-failure'],
