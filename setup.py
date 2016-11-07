64e7e2a89f43c973fc124dae39800cb165666257
David Andreoletti <david.andreoletti.dev@gmail.com>
2016-11-07 14:49:37 +0800

    Add OANDAPY as dependency for OANDA_REST datareader

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def readme():
         return f.read()
 
 INSTALL_REQUIRES = (
-    ['pandas', 'requests>=2.3.0', 'requests-file', 'requests-ftp']
+    ['pandas', 'requests>=2.3.0', 'requests-file', 'requests-ftp', 'oandapy']
 )
 
 setup(
