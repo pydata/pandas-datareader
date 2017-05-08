d5dfe74cf5020a4062860dcf1f610d749e4edb58
gfyoung <gfyoung17@gmail.com>
2017-05-07 20:02:03 -0400

    COMPAT: Make is_number import backwards-compat (#309)

--- pandas_datareader/compat/__init__.py
+++ pandas_datareader/compat/__init__.py
@@ -1,2 +1,11 @@
 # flake8: noqa
+import pandas as pd
+
 from io import BytesIO
+from distutils.version import LooseVersion
+
+pandas_version = LooseVersion(pd.__version__)
+if pandas_version <= '0.19.2':
+    from pandas.core.common import is_number
+else:
+    from pandas.api.types import is_number
