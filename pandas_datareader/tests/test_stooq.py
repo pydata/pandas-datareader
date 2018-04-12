e973cc31da24d9271a4b49423e855c7a887521b1
dtemkin <just.dantastic@yandex.com>
2018-04-12 00:40:25 -0600

    Attempting to fix stooq tests

--- pandas_datareader/tests/test_stooq.py
+++ pandas_datareader/tests/test_stooq.py
@@ -3,10 +3,10 @@ from pandas_datareader.data import get_data_stooq
 
 
 def test_stooq_dji():
-    f = web.DataReader('GS', 'stooq')
+    f = web.DataReader('GS.US', 'stooq')
     assert f.shape[0] > 0
 
 
 def test_get_data_stooq_dji():
-    f = get_data_stooq('AMZN')
+    f = get_data_stooq('AMZN.US')
     assert f.shape[0] > 0
