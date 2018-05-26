50f100a6b8987037fc7048d1a630850db63bcf1a
Travis CI User <travis@example.org>
2018-05-26 14:53:21 +0000

    Update docs after building Travis build 976 of pydata/pandas-datareader
    The docs were built from the branch 'master' against the commit
    3c1705824858f20db91563d694a93c129458bd2e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pydata/pandas-datareader/jobs/384106427.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.6.0+28.g1b3592e',
+    VERSION: '0.6.0+30.g3c17058',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
