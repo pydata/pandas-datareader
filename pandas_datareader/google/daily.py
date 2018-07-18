6b9ee11c38b5a860f87776a0f3c2ca7f1b32496b
Noah Stoffman <stoffman@gmail.com>
2018-07-18 14:49:56 -0400

    Add support for Google
    issue #551

--- pandas_datareader/google/daily.py
+++ pandas_datareader/google/daily.py
@@ -1,6 +1,9 @@
 from pandas_datareader.base import _DailyBaseReader
-from pandas_datareader.exceptions import ImmediateDeprecationError, \
-    DEP_ERROR_MSG
+import math
+import re
+from datetime import datetime
+from io import StringIO
+from pandas import read_csv
 
 
 class GoogleDailyReader(_DailyBaseReader):
@@ -20,6 +23,8 @@ class GoogleDailyReader(_DailyBaseReader):
         representations (e.g., 'JAN-01-2010', '1/1/10', 'Jan, 1, 1980')
     end : string, (defaults to today)
         Ending date, timestamp. Same format as starting date.
+    interval : integer, (defaults to 86400 = daily)
+        Time in seconds between observations.
     retry_count : int, default 3
         Number of times to retry query request.
     pause : int, default 0
@@ -31,24 +36,51 @@ class GoogleDailyReader(_DailyBaseReader):
         requests.sessions.Session instance to be used
     """
 
-    def __init__(self, symbols=None, start=None, end=None, retry_count=3,
-                 pause=0.001, session=None, chunksize=25):
-        raise ImmediateDeprecationError(DEP_ERROR_MSG.format('Google finance'))
+    def __init__(self, symbols=None, start=None, end=None, interval=86400,
+                 retry_count=3, pause=0.001, session=None, chunksize=25):
         super(GoogleDailyReader, self).__init__(symbols, start, end,
                                                 retry_count, pause, session,
                                                 chunksize)
 
+        self.interval = interval
+
     @property
     def url(self):
-        """API URL"""
-        # Thanks user:vnmabus for pointing this out.
-        return 'https://finance.google.co.uk/bctzjpnsun/historical'
+        return 'https://finance.google.com/finance/getprices'
 
     def _get_params(self, symbol):
-        params = {
-            'q': symbol,
-            'startdate': self.start.strftime('%b %d, %Y'),
-            'enddate': self.end.strftime('%b %d, %Y'),
-            'output': "csv"
-        }
+        # get data for years covering requested period (fuzzed to prevent zero)
+        years_to_get = math.ceil((datetime.today() - self.start).days
+                                 / 365.25 + 0.0001)
+
+        params = {'q': symbol,
+                  'i': self.interval,
+                  'p': str(years_to_get) + 'Y'}
         return params
+
+    def _read_one_data(self, url, params):
+
+        resp = self._get_response(url, params=params)
+
+        colnames = re.search(r'COLUMNS=(.+)', resp.text).group(1).split(',')
+        colnames = list(map(str.capitalize, colnames))
+
+        data = re.search(r'DATA=\n(.*)', resp.text, re.DOTALL).group(1)
+        prices = read_csv(StringIO(data), names=colnames, header=None)
+        prices = prices[~prices['Date'].str.contains('TIMEZONE_OFFSET')]
+
+        dates = []
+        for dt in prices['Date']:
+            if 'a' in dt:
+                base = int(dt[1:])
+                date = base
+            else:
+                date = base + int(dt)*params['i']
+            dates.append(date)
+
+        prices.index = dates
+        prices.index = prices.index.map(datetime.fromtimestamp)
+        prices = prices.drop('Date', axis=1)
+        prices = prices[self.start:self.end]
+
+        return prices
