647892b6ce45be2fc7fd78cfc7866a9e1012982a
Bradley <mustyoshi@gmail.com>
2018-08-23 21:58:53 -0400

    Tests

--- pandas_datareader/tests/test_stooq.py
+++ pandas_datareader/tests/test_stooq.py
@@ -3,10 +3,14 @@ from pandas_datareader.data import get_data_stooq
 
 
 def test_stooq_dji():
-    f = web.DataReader('GS', 'stooq')
+    f = web.DataReader('GS.us', 'stooq')
     assert f.shape[0] > 0
 
 
 def test_get_data_stooq_dji():
     f = get_data_stooq('AMZN')
     assert f.shape[0] > 0
+
+def test_get_data_stooq_dates():
+    f = get_data_stooq('SPY', start='20180101', end='20180115')
+    assert f.shape[0] == 15
