a336a8efeab0449b10d4cbcb09e2ee8e54b8cfea
addisonlynch <ahlshop@gmail.com>
2019-01-21 15:38:56 -0500

    Remove google

--- pandas_datareader/__init__.py
+++ pandas_datareader/__init__.py
@@ -1,12 +1,11 @@
 from ._version import get_versions
 from .data import (DataReader, Options, get_components_yahoo,
                    get_dailysummary_iex, get_data_enigma, get_data_famafrench,
-                   get_data_fred, get_data_google, get_data_moex,
+                   get_data_fred, get_data_moex,
                    get_data_morningstar, get_data_quandl, get_data_stooq,
                    get_data_yahoo, get_data_yahoo_actions, get_iex_book,
                    get_iex_symbols, get_last_iex, get_markets_iex,
-                   get_nasdaq_symbols,
-                   get_quote_google, get_quote_yahoo, get_recent_iex,
+                   get_nasdaq_symbols, get_quote_yahoo, get_recent_iex,
                    get_records_iex, get_summary_iex, get_tops_iex,
                    get_data_tiingo, get_data_alphavantage)
 
@@ -14,8 +13,8 @@ __version__ = get_versions()['version']
 del get_versions
 
 __all__ = ['__version__', 'get_components_yahoo', 'get_data_enigma',
-           'get_data_famafrench', 'get_data_google', 'get_data_yahoo',
-           'get_data_yahoo_actions', 'get_quote_google', 'get_quote_yahoo',
+           'get_data_famafrench', 'get_data_yahoo',
+           'get_data_yahoo_actions', 'get_quote_yahoo',
            'get_iex_book', 'get_iex_symbols', 'get_last_iex',
            'get_markets_iex', 'get_recent_iex', 'get_records_iex',
            'get_summary_iex', 'get_tops_iex',
--- pandas_datareader/tests/test_data.py
+++ pandas_datareader/tests/test_data.py
@@ -2,16 +2,9 @@ import pytest
 
 from pandas import DataFrame
 from pandas_datareader.data import DataReader
-from pandas_datareader.exceptions import UnstableAPIWarning
 
 
 class TestDataReader(object):
-
-    @pytest.mark.xfail(reason="Deprecated")
-    def test_read_google(self):
-        with pytest.warns(UnstableAPIWarning):
-            gs = DataReader("GS", "google")
-            assert isinstance(gs, DataFrame)
 
     def test_read_iex(self):
         gs = DataReader("GS", "iex-last")
