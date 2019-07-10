47af0ce03b786304dc06c47efea14e01585698ef
simongarisch <gatman946@gmail.com>
2019-07-11 07:19:29 +1000

    Modified test_nasdaq

--- pandas_datareader/tests/test_nasdaq.py
+++ pandas_datareader/tests/test_nasdaq.py
@@ -1,8 +1,12 @@
+import pandas_datareader
 import pandas_datareader.data as web
+from pandas_datareader._testing import skip_on_exception
 
 
 class TestNasdaqSymbols(object):
 
+    @skip_on_exception(RemoteDataError)
     def test_get_symbols(self):
+        pandas_datareader.nasdaq_trader._ticker_cache = None
         symbols = web.DataReader('symbols', 'nasdaq')
         assert 'IBM' in symbols.index
