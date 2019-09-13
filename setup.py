09a5df0b4bf3681f941f71c9990987b2999e9429
Gábor Lipták <gliptak@gmail.com>
2019-09-12 20:03:34 -0400

    List Python 3.7 in setup

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
     ],
     keywords='data',
