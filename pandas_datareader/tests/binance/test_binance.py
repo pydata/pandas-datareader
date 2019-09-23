1e2203088d88bd6b9cfb39eba0a4f8de2376fbf3
Rohit Jain <rohitjain18005@gmail.com>
2019-09-23 07:07:13 +0530

    flake8 reformatted

--- pandas_datareader/tests/binance/test_binance.py
+++ pandas_datareader/tests/binance/test_binance.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import pytest
 import pandas_datareader as pdr
-import datetime
 
 pytestmark = pytest.mark.stable
 
@@ -13,12 +12,11 @@ class TestBinance(object):
 
     def test_object_type(self):
         assert isinstance(self.binance_reader, pd.DataFrame)
-    
+
     def test_object_dimensions(self):
         assert self.binance_reader.shape[1] == len(self.columns_should_be_present), "Number of columns in the returned data is higher than 11"
         assert self.binance_reader.shape[0] <= self.limit, "Number of rows in data returned is higher than the expected number"
-    
+
     def test_object_columns(self):
         for col in self.binance_reader.columns:
             assert col in self.columns_should_be_present, ("Column" + col + "should not be present in the response")
-        
