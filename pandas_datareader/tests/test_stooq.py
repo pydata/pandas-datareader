480006cd687e1fa11acc1982a779adaadb200b6d
kdcro101 <kdomagoj2@gmail.com>
2019-10-01 14:57:31 +0000

    test for futures

--- pandas_datareader/tests/test_stooq.py
+++ pandas_datareader/tests/test_stooq.py
@@ -25,6 +25,10 @@ def test_stooq_sp500():
     f = get_data_stooq("^SPX")
     assert f.shape[0] > 0
 
+def test_stooq_clx19f():
+    f = get_data_stooq("CLX19.F", start="20190101", end="20190115")
+    assert f.shape[0] > 0
+
 
 def test_get_data_stooq_dax():
     f = get_data_stooq("^DAX")
@@ -39,3 +43,5 @@ def test_stooq_googl():
 def test_get_data_ibm():
     f = get_data_stooq("IBM.UK")
     assert f.shape[0] > 0
+
+ 
