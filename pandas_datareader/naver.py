c4d36c0be4be23b2e6fa7d8c680b358b53676267
Sumin Byeon <suminb@gmail.com>
2019-10-08 00:57:34 +0900

    Parse XML data

--- pandas_datareader/naver.py
+++ pandas_datareader/naver.py
@@ -1,3 +1,6 @@
+from xml.etree import ElementTree
+
+from pandas import DataFrame
 from pandas_datareader.base import _DailyBaseReader
 
 class NaverDailyReader(_DailyBaseReader):
@@ -51,6 +54,31 @@ class NaverDailyReader(_DailyBaseReader):
         return params
 
     def _read_one_data(self, url, params):
-        """Read one data from specified symbol"""
+        """Read one data from specified symbol.
+
+        :rtype: DataFrame
+        """
         resp = self._get_response(url, params=params)
-        return resp
+        parsed = self._parse_xml_response(resp.text)
+        prices = DataFrame(parsed, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+
+        return prices
+
+    def _parse_xml_response(self, xml_content):
+        """Parses XML response from the server.
+
+        An example of response:
+
+            <?xml version="1.0" encoding="EUC-KR" ?>
+            <protocol>
+                <chartdata symbol="005930" name="삼성전자" count="500" timeframe="day" precision="0" origintime="19900103">
+                    <item data="20170918|218500|222000|217000|220500|72124" />
+                    <item data="20170919|218000|221000|217500|219000|62753" />
+                    ...
+            </protocol>
+        """
+        root = ElementTree.fromstring(xml_content)
+        items = root.findall("chartdata/item")
+
+        for item in items:
+            yield item.attrib["data"].split("|")
