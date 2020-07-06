af8eb92f8dc7a36486f2764108f8808e1249c33e
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-07-06 14:24:49 +0000

    Update docs after building Travis build 1422 of pydata/pandas-datareader
    The docs were built from the branch 'master' against the commit
    620ce46bb8a75d029c1780dd0a5969d8d10ee4ee.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pydata/pandas-datareader/jobs/705426785.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.6/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.0dev0+47.ge452bf8',
+    VERSION: '0.9.0dev0+61.g620ce46',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
