03a2cfb7a15d7d82b80303bcd542f85002ac996c
yancong <32220263+ice-tong@users.noreply.github.com>
2021-02-10 21:10:50 +0800

    Fix  `get_data_yahoo()` Bug in windows when satrt date is earlier than 1970-1-1 8:00
    Bug reproduce: 
    ``` python3
    from pandas_datareader import data as web
    web.get_data_yahoo("IBM", start='1968-02-28', end='2020-02-08', interval="d")
    ```
    ERROR `Overflow error: mktime argument out of range`

--- pandas_datareader/yahoo/daily.py
+++ pandas_datareader/yahoo/daily.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 import json
 import re
-import time
+import time, datetime
 
 from pandas import DataFrame, isnull, notnull, to_datetime
 
@@ -127,7 +127,11 @@ class YahooDailyReader(_DailyBaseReader):
     def _get_params(self, symbol):
         # This needed because yahoo returns data shifted by 4 hours ago.
         four_hours_in_seconds = 14400
-        unix_start = int(time.mktime(self.start.timetuple()))
+        unix_zero = datetime.datetime(1970, 1, 1, 8)
+        if self.start < unix_zero:
+            unix_start = int((self.start - unix_zero).total_seconds())
+        else:
+            unix_start = int(time.mktime(self.start.timetuple()))
         unix_start += four_hours_in_seconds
         day_end = self.end.replace(hour=23, minute=59, second=59)
         unix_end = int(time.mktime(day_end.timetuple()))
