2e99cea9cb9bb967b1664c653339107e5f78d15d
yancong <32220263+ice-tong@users.noreply.github.com>
2021-02-10 23:04:47 +0800

    meet flake8 formart

--- pandas_datareader/yahoo/daily.py
+++ pandas_datareader/yahoo/daily.py
@@ -128,8 +128,6 @@ class YahooDailyReader(_DailyBaseReader):
     def _get_params(self, symbol):
         # This needed because yahoo returns data shifted by 4 hours ago.
         four_hours_in_seconds = 14400
-        # Use the difference between the unix_zero and start to prevent the bug occurrence of
-        # `OverflowError: mktime argument out of range`.
         unix_zero = datetime.datetime(1970, 1, 1, 8)
         unix_start = int((self.start - unix_zero).total_seconds())
         unix_start += four_hours_in_seconds
