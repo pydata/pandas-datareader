bb67ab10b724f2d4fb5f17616d020c13ef875726
begoat <hylbegoat@gmail.com>
2021-08-02 14:55:14 +0800

    test(test_data.py): add testcase for custom datasource

--- pandas_datareader/tests/test_data.py
+++ pandas_datareader/tests/test_data.py
@@ -1,7 +1,8 @@
 from pandas import DataFrame
 import pytest
 
-from pandas_datareader.data import DataReader
+from pandas_datareader.base import _DailyBaseReader
+from pandas_datareader.data import DataReader, register_custom_datareader
 
 pytestmark = pytest.mark.stable
 
@@ -18,3 +19,43 @@ class TestDataReader(object):
     def test_not_implemented(self):
         with pytest.raises(NotImplementedError):
             DataReader("NA", "NA")
+
+    def test_custom_reader_acc(self):
+        class DemoReader(_DailyBaseReader):
+            def __init__(
+                self,
+                symbols=None,
+                start=None,
+                end=None,
+                retry_count=3,
+                pause=0.1,
+                session=None,
+                api_key=None,
+            ):
+                super().__init__(
+                    symbols=symbols,
+                    start=start,
+                    end=end,
+                    retry_count=retry_count,
+                    pause=pause,
+                    session=session,
+                )
+
+            @property
+            def url(self):
+                return 'https://stooq.com/q/d/l/'
+
+            def _get_params(self, symbol):
+                params = {
+                    's': symbol,
+                    'i': "d"
+                }
+                return params
+
+        register_custom_datareader('demo', DemoReader)
+        result = DataReader('USDJPY', 'demo')
+        assert isinstance(result, DataFrame)
+
+    def test_custom_reader_fail(self):
+        with pytest.raises(NotImplementedError):
+            DataReader('USDJPY', 'demo1')
